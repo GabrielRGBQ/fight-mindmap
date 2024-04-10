37b7090f277c99457f03c5e5fdb06d97def5dcd0
Gabriel <gabri.dlq@gmail.com>
2024-04-10 13:42:14 +0200

    Create first sqlalchemy model and connect to db

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from .routers import user
+from .model import models
+from .database import engine
+
+
+# Create the tables in the database if they don't already exist
+# It will be needed no longer when we start using alembic
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(user.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-app.include_router(user.router)
---
+++ app/model/models.py
@@ -0,0 +1,14 @@
+from ..database import Base
+from sqlalchemy import TIMESTAMP, Column, Integer, String
+from sqlalchemy.sql.expression import text
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
