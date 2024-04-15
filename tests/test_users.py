5b2e11c9dacf968ff3eba0518e7d24a6ed929450
Gabriel <gabri.dlq@gmail.com>
2024-04-15 08:58:17 +0200

    Use testclient instead

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,19 @@
-import requests
+# import requests
 from app.config import settings
-# from fastapi.testclient import TestClient
-# from app.main import app
+from fastapi.testclient import TestClient
+from app.main import app
 
 URL = f'http://localhost:{settings.listening_port}/users/'
 
-def test_create_user():
-    data = {"name": "string",
-            "email": "user@example.com"}
-    response = requests.post(url=URL, json=data)
-    assert response.status_code == 200
-
-# client = TestClient(app)
-
-# def test_create_user_testclient():
-#     data = {"name": "string", "email": "user@example.com"}
-#     response = client.post("/users/", json=data)
+# def test_create_user():
+#     data = {"name": "string",
+#             "email": "user@example.com"}
+#     response = requests.post(url=URL, json=data)
 #     assert response.status_code == 200
+
+client = TestClient(app)
+
+def test_create_user_testclient():
+    data = {"name": "string", "email": "user@example.com"}
+    response = client.post("/users/", json=data)
+    assert response.status_code == 200
