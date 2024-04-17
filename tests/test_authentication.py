5a1618664a848f3370d9dae168975133164856f3
Gabriel <gabri.dlq@gmail.com>
2024-04-17 09:26:44 +0200

    Test different scenarios for authentication

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -16,3 +16,17 @@ def test_login_user(client, test_user):
     assert user_id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("hello123@gmail.com", "wrongpassword123", 403),
+        ("wrongemail@gmail.com", "wrongpassword123", 403),
+        (None, "password123", 422),
+        ("hello123@gmail.com", None, 422),
+    ],
+)
+def test_incorrect_login(client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
